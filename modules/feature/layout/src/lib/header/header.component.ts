2fa69b1142959349425805f966753733fe46f2b9
Kelvis Borges <kelvisbcc@gmail.com>
2024-03-02 02:13:12 -0300

    feat(layout): add header

---
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ecommerce-angular-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
