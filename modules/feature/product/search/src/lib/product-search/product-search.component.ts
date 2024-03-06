06c9c246c40984c8309284cefcaf5888c955291a
Kelvis Borges <kelvisbcc@gmail.com>
2024-03-05 23:50:20 -0300

    feat(product-search): add search to header

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { mockProducts } from '@ecommerce-angular/product-data-access';
 
 @Component({
   selector: 'ecommerce-angular-product-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+  ],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {}
+export class ProductSearchComponent {
+  control = new FormControl('');
+  products = mockProducts;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LayoutModule } from '@ecommerce-angular/layout';
+import { ProductSearchComponent } from '@ecommerce-angular/product-search';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    LayoutModule,
+    ProductSearchComponent,
+  ],
   selector: 'ecommerce-angular-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
