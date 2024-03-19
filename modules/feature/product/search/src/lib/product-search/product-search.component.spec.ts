91761e9b5cc5d249c9fbcd554535628772d79aa8
Kelvis Borges <kelvisbcc@gmail.com>
2024-03-18 22:46:21 -0300

    test(product-search): add tests to component

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,11 +1,22 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  ProductSearchService,
+  mockProducts,
+} from '@ecommerce-angular/product-data-access';
+import { of } from 'rxjs';
 import { ProductSearchComponent } from './product-search.component';
 
 describe('ProductSearchComponent', () => {
   let component: ProductSearchComponent;
   let fixture: ComponentFixture<ProductSearchComponent>;
+  let productSearchService: ProductSearchService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,8 +25,15 @@ describe('ProductSearchComponent', () => {
         NoopAnimationsModule,
         HttpClientTestingModule,
       ],
+      providers: [
+        {
+          provide: ProductSearchService, // Quando o componente visualizar esse serviceo
+          useValue: { searchByName: () => of(mockProducts) }, // na verdade ele vai utilizar essa implementação
+        },
+      ],
     }).compileComponents();
 
+    productSearchService = TestBed.inject(ProductSearchService);
     fixture = TestBed.createComponent(ProductSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,5 +41,72 @@ describe('ProductSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should debounce when input field is changed', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    input.value = 'tv';
+    input.dispatchEvent(new Event('input'));
+
+    expect(productSearchService.searchByName).not.toHaveBeenCalled();
+
+    tick(500);
+
+    expect(productSearchService.searchByName).toHaveBeenCalledWith('tv');
+  }));
+
+  it('should search multiple times', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    input.value = 'tv';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    input.value = 'notebook';
+    input.dispatchEvent(new Event('input'));
+    tick(500);
+
+    expect(productSearchService.searchByName).toHaveBeenCalledTimes(2);
+  }));
+
+  it('should prevent identical submissions', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    input.value = 'tv';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    input.value = 'tv';
+    input.dispatchEvent(new Event('input'));
+    tick(500);
+
+    expect(productSearchService.searchByName).toHaveBeenCalledTimes(1);
+  }));
+  it('should prevent empty submissions', fakeAsync(() => {
+    jest.spyOn(productSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    input.value = '';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    expect(productSearchService.searchByName).not.toHaveBeenCalled();
+  }));
+
+  it('should return products observable correctly', () => {
+    component.products$.subscribe((products) => {
+      expect(products).toEqual(mockProducts);
+    });
   });
 });
--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -38,7 +38,7 @@ export class ProductSearchComponent implements OnInit {
     this.products$ = this.control.valueChanges.pipe(
       debounceTime(500),
       distinctUntilChanged(),
-      filter((text) => text.length > 1),
+      filter((text) => text.length > 0),
       switchMap((text) => this.productSearchService.searchByName(text))
     );
   }
