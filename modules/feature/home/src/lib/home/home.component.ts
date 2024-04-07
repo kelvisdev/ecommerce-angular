d7785c0cd89a7deaf1765d12cbd3dc2083e410d0
Kelvis Borges <kelvisbcc@gmail.com>
2024-04-07 16:06:31 -0300

    feat(home): add recommended products

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { mockProducts } from '@ecommerce-angular/product-data-access';
 
 @Component({
   selector: 'ecommerce-angular-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products = mockProducts;
+}
