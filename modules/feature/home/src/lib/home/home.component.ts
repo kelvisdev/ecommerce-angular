e417fadc8ff2128227b9c62f533d35778afed480
Kelvis Borges <kelvisbcc@gmail.com>
2024-04-07 18:59:10 -0300

    refactor(home): use product card component

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+
 import { RecommendedProductsService } from '@ecommerce-angular/product-data-access';
+import { ProductCardComponent } from '@ecommerce-angular/product-ui';
 
 @Component({
   selector: 'ecommerce-angular-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- modules/ui/product/src/lib/components/product-card/product-card.component.ts
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '@ecommerce-angular/product-data-access';
 
 @Component({
   selector: 'ecommerce-angular-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  @Input() product!: Product;
+}
