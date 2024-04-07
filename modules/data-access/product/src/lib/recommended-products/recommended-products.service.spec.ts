363f98aae1dc2f9e9102a4e42b60b9f5529ed93d
Kelvis Borges <kelvisbcc@gmail.com>
2024-04-07 17:55:16 -0300

    feat(product-data-access): add recommended products

---
+++ modules/data-access/product/src/lib/recommended-products/recommended-products.service.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from '@angular/core/testing';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { mockProducts } from '../mocks/product.mock';
+import { Product } from '../models/product.model';
+import { RecommendedProductsService } from './recommended-products.service';
+
+describe('RecommendedProductsService', () => {
+  let service: RecommendedProductsService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(RecommendedProductsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return recommended products correctly', () => {
+    // Arrange
+    const mockName = 'notebook';
+    const url = `${service.apiUrl}/products?page=1&limit=6`;
+    let result: Product[] = [];
+
+    // Act
+    service.getProducts().subscribe((products) => (result = products));
+
+    // Assert
+    const request = httpMock.expectOne(url);
+    request.flush(mockProducts);
+    expect(request.request.method).toBe('GET');
+    expect(result).toEqual(mockProducts);
+  });
+});
